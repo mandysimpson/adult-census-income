03c41270448fd497f06beb5b7b2bd696da835576
Mandy Simpson <mandy@simpson.org.nz>
2019-10-20 13:55:13 +1300

    initial analysis complete

--- ACIProject.R
+++ ACIProject.R
@@ -1,10 +1,14 @@
-library(tidyverse)
-library(caret)
-library(dplyr)
+#install packages
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
+if(!require(rpart)) install.packages("rpart", repos = "http://cran.us.r-project.org")
+if(!require(randomForest)) install.packages("randomForest", repos = "http://cran.us.r-project.org")
 
+#download file
+adult_census_income <- read.csv("https://github.com/mandysimpson/adult-census-income/raw/master/adult.csv")
 
-adult_census_income <- read.csv("adult.csv")
-
+#dimensions
 nrows(adult_census_income)
 ncols(adult_census_income)
 
@@ -46,25 +50,19 @@ aci %>% ggplot(aes(hours.per.week, y = ..count.., fill = income)) + geom_histogr
 
 
 #splitting the dataset for validation and training
-
-set.seed(1,sample.kind = "Rounding")
-#if using R3.5 or earlier set.seed(1)
+set.seed(1,sample.kind = "Rounding")  #if using R3.5 or earlier set.seed(1)
 test_index <- createDataPartition(aci$income, times = 1, p = 0.2, list = FALSE)
 validation <- aci[test_index, ]
-training <- aci[-test_index, ]
+aci_training <- aci[-test_index, ]
 
 #splitting the dataset again for testing
-set.seed(10,sample.kind = "Rounding")
-#if using R3.5 or earlier set.seed(10)
-
-test_index2 <- createDataPartition(training$income, times = 1, p = 0.2, list = FALSE)
-testing <- training[test_index2, ]
-training <- training[-test_index2, ]
+set.seed(10,sample.kind = "Rounding")  #if using R3.5 or earlier set.seed(10)
+test_index2 <- createDataPartition(aci_training$income, times = 1, p = 0.2, list = FALSE)
+testing <- aci_training[test_index2, ]
+training <- aci_training[-test_index2, ]
 
 #knn algorithm
-
-set.seed(3,sample.kind = "Rounding")
-#if using R3.5 or earlier set.seed(3)
+set.seed(3,sample.kind = "Rounding")  #if using R3.5 or earlier set.seed(3)
 control <- trainControl(method = "cv", number = 10, p = .9)
 train_knn <- train(income ~ ., 
                    method = "knn", 
@@ -73,6 +71,21 @@ train_knn <- train(income ~ .,
                    trControl = control)
 ggplot(train_knn,highlight = TRUE)
 train_knn$bestTune
-
 confusionMatrix(predict(train_knn, testing, type = "raw"), 
                 testing$income)$overall["Accuracy"]
+
+#classification tree algorithm
+set.seed(3,sample.kind = "Rounding")  #if using R3.5 or earlier set.seed(3)
+train_rpart <- train(income ~ ., method = "rpart", tuneGrid = data.frame(cp = seq(0.0, 0.1, len=25)), data = training)
+plot(train_rpart)
+train_rpart$bestTune
+confusionMatrix(predict(train_rpart, testing), testing$income)$overall["Accuracy"]
+plot(train_rpart$finalModel, margin = 0.1)
+text(train_rpart$finalModel, cex = 0.75)
+
+#random forest algorithm
+set.seed(3,sample.kind = "Rounding")  #if using R3.5 or earlier set.seed(3)
+train_rf <- randomForest(income ~ ., data = training)
+confusionMatrix(predict(train_rf, testing), testing$income)$overall["Accuracy"]
+importance(train_rf)
+
